b1bc8547af81d7abe43d22580a3bdce040bf9115
mehakkalra1403 <mehak.kalra@mail.utoronto.ca>
2020-04-09 23:16:30 +0530

    New board drawn

--- Draw board.c
+++ Draw board.c
@@ -1,78 +1,225 @@
+//#include <cstdlib>
 volatile int pixel_buffer_start; // global variable
+
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
-#include <math.h>
-#include <stdlib.h>	
-int main(void)
-{
+
+/* Function Declarations: */
+ 
+#define BOARD_SIZE 6
+
+int piecesRemaining[BOARD_SIZE]; //keep track of the number of game pieces that still exist in each row
+
+void resetMatch(bool * turn, bool * gameOver, char * continueGame, int * piecesRemaining, int row[BOARD_SIZE][BOARD_SIZE]);
+bool removePieces(int rowNo, int noPieces, int row[BOARD_SIZE][BOARD_SIZE]);
+bool checkWin();
+void print(int row[BOARD_SIZE][BOARD_SIZE]);
+void updateRounds(bool turn, int * playerOneScore, int * playerTwoScore);
+void printPieces(int piecesRemaining, int rowNo);
+draw_box(int Xstart, int Xend, int Ystart, int Yend, int color);
+				 
+				 
+int main() {
+    //Initial setup
+    int rowNo;
+    int noPieces;
+    int row[BOARD_SIZE][BOARD_SIZE];
+    bool turn;
+    bool gameOver;
+    char continueGame;
+    int playerOneScore = 0, playerTwoScore = 0; //initial scores
+	
+	
+	
+	
 	volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
-    /* Read location of the pixel buffer from the pixel buffer controller */
     pixel_buffer_start = *pixel_ctrl_ptr;
 	clear_screen();
-	draw();
-	video_text(20,5,"NIM - STAY TILL THE LAST AND COME FIRST");
-}
+	clear_char();
+	video_text(22,5,"NIM - STAY TILL THE LAST AND COME FIRST");
+	//printPieces(2,3);
+	//draw_box(189,209,99,119,0x0F1F);
 
-void video_text(int x, int y, char * text_ptr) {
-	int offset;
-	volatile char * character_buffer = (char*)0xC9000000;
-	offset = (y << 7) + x; 
-	while (*(text_ptr)) {
-		*(character_buffer + offset) = *(text_ptr); // write to the character buffer
-		++text_ptr;
-		++offset;
+    resetMatch (&turn, &gameOver, &continueGame, piecesRemaining, row);
+   
+    while(continueGame == 'y' && !gameOver)
+    {
+        print(row);
+        bool invalidMove = true;
+        while(invalidMove){
+            printf( "Player %d: Which row would you like to remove pieces from? \n ", ((turn) ? 1 : 2));
+            scanf( "%d", &rowNo);
+                
+            printf( " Player %d: How many pieces would you like to remove from %d? \n ",((turn) ? 1 : 2), rowNo);
+            scanf( "%d", &noPieces);
+            
+            invalidMove = removePieces(rowNo, noPieces ,row);
+
+        }
+
+        if (checkWin(row)){
+            print(row);
+
+            printf("Player %d Wins the match!\n", ((turn) ? 1 : 2));
+            gameOver = true;
+            updateRounds(turn, &playerOneScore, &playerTwoScore);
+            
+            printf("Play Again? Press 'y' and hit ENTER");
+            scanf(" %c", &continueGame);   
+            if (continueGame == 'y')
+                resetMatch (&turn, &gameOver, &continueGame, piecesRemaining, row);
+        }
+        else{
+            turn = turn^1; //toggle turn        
+            printf("Continue? Press 'y' and hit ENTER");
+            scanf(" %c", &continueGame);  
+        }
+                
+    }
+	
+	
+    //game finished or user exited
+    
+    if (continueGame != 'y')
+        printf("User Exited Game");
+    
+    return 0;
+        
+} 
+
+void resetMatch(bool * turn, bool * gameOver, char * continueGame, int * piecesRemaining, int row[BOARD_SIZE][BOARD_SIZE]){
+    
+    *turn = true;
+    *gameOver = false;
+    *continueGame = 'y';
+    
+    //Randomize the distibution of the 21 pieces across the piles
+    int totalCount = 21;
+    //initliaze the # of game pieces in each row
+    /*int randomNumber;
+    for (int i = 0; i < BOARD_SIZE && totalCount > 0; i++){
+        randomNumber = rand()%7; //random number from 0 to 6
+        piecesRemaining[i] = randomNumber; 
+        totalCount-=randomNumber;
+		printPieces(piecesRemaining[i],i);
+    }*/
+	for (int i=0; i<6; i++){
+        
+        piecesRemaining[i] = i+3; 
+        
+		printPieces(piecesRemaining[i],i);
+		//printPieces(6,4);
+	
 	}
+	
+    //for each board's row, set the correct number of pieces 
+    for (int i = 0; i < BOARD_SIZE;i++)
+    {
+        for(int j = 0; j < piecesRemaining[i]; j++)
+        {
+            row[i][j] = 1;
+        }
+    }
+    
+    for (int i = 0; i < BOARD_SIZE;i++)
+    {
+        for(int j = piecesRemaining[i]; j < BOARD_SIZE; j++)
+        {
+            row[i][j] = 0;
+        }
+    }
+	
+	
+    
+}
+        
+        
+bool removePieces(int rowNo, int noPieces, int row[BOARD_SIZE][BOARD_SIZE])
+{
+    rowNo--;
+    if (rowNo<0 || rowNo >= BOARD_SIZE){
+        printf("Invalid row #");
+        return true;
+    }
+    int piecesLeftInRow = piecesRemaining[rowNo];
+    if (noPieces > piecesLeftInRow || noPieces < 1){
+        printf("Invalid # of pieces");
+        return true;
+    }
+    for (int k = piecesLeftInRow; k >= piecesLeftInRow - noPieces; k--) //update board display
+    {
+        row[rowNo][k] = 0;
+    }
+    piecesRemaining[rowNo]-=noPieces; //update the record of # of pieces left in board
+    return false;
+}
+
+bool checkWin()
+{
+    for (int i =0; i< BOARD_SIZE;i++)
+    {
+        if (piecesRemaining[i] != 0) // if a single piece is left
+            return false;
+    }
+    return true; //empty board (no pieces left)
+}
+
+void print(int row[BOARD_SIZE][BOARD_SIZE])
+{
+    for (int i =0; i< BOARD_SIZE;i++)
+    {   
+        printf("Row %d\t", (i+1));
+        for(int j = 0;j < BOARD_SIZE; j++)
+        {
+            printf("%d ",row[i][j]);
+                    
+        }
+        
+        printf("\n");
+    }
+}        
+      
+void updateRounds(bool turn, int * playerOneScore, int * playerTwoScore){
+    bool resetRounds = false;
+    if (turn) {
+        (*playerOneScore)++;
+        if ((*playerOneScore)>=5){
+            printf("Player One Wins the Round");
+            resetRounds = true;
+        }
+    }
+    else  {
+        (*playerTwoScore)++;
+        if ((*playerTwoScore)>=5){
+            printf("Player Two Wins the Round");
+            resetRounds = true;
+        }
+    }
+    if (resetRounds){
+        (*playerOneScore) = 0;
+        (*playerTwoScore) = 0;
+    }
 }
 
 
-	
-draw()
+void printPieces(int piecesRemaining, int rowNo)
 {
-	/*for(int i =0;i<319;i++)
+	bool black = false;
+	for(int i=89;i<piecesRemaining*2*20;i+=20)
 	{
-		for(int j=0; j<239;j++)
-		{
-			draw_box(99,219);
+		if (!black){
+			draw_box(i,i+20,((rowNo+1)*30+9), ((rowNo+1)*30)+29,0x07E0);
+			//draw_box(i,i+20,30+ 9, 30+ 9 +20,0x07E0);
+			 black = true;
 		}
-	}*/
-	draw_box(149,169,39,59,0x76F6);
-	
-	draw_box(129,149,69,89,0xF800);
-	draw_box(169,189,69,89,0xF800);
-	
-	draw_box(109,129,99,119,0x0F1F);
-	draw_box(149,169,99,119,0x0F1F);
-	draw_box(189,209,99,119,0x0F1F);
-	
-	draw_box(89,109,129,149,0xF81F);
-	draw_box(129,149,129,149,0xF81F);
-	draw_box(169,189,129,149,0xF81F);
-	draw_box(209,229,129,149,0xF81F);
-	
-	draw_box(69,89,159,179,0x07E0);
-	draw_box(109,129,159,179,0x07E0);
-	draw_box(149,169,159,179,0x07E0);
-	draw_box(189,209,159,179,0x07E0);
-	draw_box(229,249,159,179,0x07E0);
-	
-	draw_box(49,69,189,209,0x888F);
-	draw_box(89,109,189,209,0x888F);
-	draw_box(129,149,189,209,0x888F);
-	draw_box(169,189,189,209,0x888F);
-	draw_box(209,229,189,209,0x888F);
-	draw_box(249,269,189,209,0x888F);
-	/*for(int i = 109; i<209; i+=20)
-	{	
-		//if(col)
-		//{
-			draw_box(i,i+20,99,129,0x001F);
-			//col = false;
-		//}
-		else
-		{
-			draw_box(i,i+20,99,129,0x001F);
-			col = true;
-		}
-	}*/
+		else{
+			 //draw_box(89,i+20,(rowNo*30)+ 9, (rowNo*30)+29,0xF800);
+			  black = false;
+	    }
+		
+	}
+
 }
 
 draw_box(int Xstart, int Xend, int Ystart, int Yend, int color)
@@ -85,8 +232,19 @@ draw_box(int Xstart, int Xend, int Ystart, int Yend, int color)
 			plot_pixel(i,j,color);
 		}
 	}
+}		
+
+void video_text(int x, int y, char * text_ptr) {
+	int offset;
+	volatile char * character_buffer = (char*)0xC9000000;
+	offset = (y << 7) + x; 
+	while (*(text_ptr)) {
+		*(character_buffer + offset) = *(text_ptr); // write to the character buffer
+		++text_ptr;
+		++offset;
+	}
 }
-	
+			  
 clear_screen()
 {
 	for(int x=0; x<=319;x++)
@@ -96,9 +254,24 @@ clear_screen()
 	}
 }
 	
+clear_char()
+{
+	for(int x=0; x<=79;x++)
+	{
+		for(int y=0;y<=59;y++)
+		video_text(x,y," ");
+	}
+}
+
+
 void plot_pixel(int x, int y, short int line_color)
 {
     *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
 }
 
+		
+		
+		
+	
+	
 	
