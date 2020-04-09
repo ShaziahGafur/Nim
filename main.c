b32636c763811f23d2a4cc24f1c9e77baa49c2aa
Shaziah Gafur <shaziah_g7@hotmail.com>
2020-04-09 13:52:14 -0400

    proceed game with ENTER

--- main.c
+++ main.c
@@ -7,12 +7,13 @@
 #define BOARD_SIZE 6
 
 int piecesRemaining[BOARD_SIZE]; //keep track of the number of game pieces that still exist in each row
-void resetMatch(bool * turn, bool * gameOver, char * continueGame, int * piecesRemaining, int row[BOARD_SIZE][BOARD_SIZE]);
+void resetMatch(bool * turn, bool * gameOver, bool * continueGame, int * piecesRemaining, int row[BOARD_SIZE][BOARD_SIZE]);
 bool removePieces(int rowNo, int noPieces, int row[BOARD_SIZE][BOARD_SIZE]);
 bool checkWin();
 void print(int row[BOARD_SIZE][BOARD_SIZE]);
 int receivedInput();
 void updateRounds(bool turn, int * playerOneScore, int * playerTwoScore);
+bool proceedGame() ;
 
 int main() {
   //Initial setup
@@ -21,12 +22,12 @@ int main() {
   int row[BOARD_SIZE][BOARD_SIZE];
   bool turn;
   bool gameOver;
-  char continueGame;
+  bool continueGame;
   int playerOneScore = 0, playerTwoScore = 0; //initial scores
 
   resetMatch( & turn, & gameOver, & continueGame, piecesRemaining, row);
 
-  while (continueGame == 'y' && !gameOver) {
+  while (continueGame && !gameOver) {
     print(row);
     bool invalidMove = true;
     while (invalidMove) {
@@ -50,19 +51,21 @@ int main() {
       updateRounds(turn, & playerOneScore, & playerTwoScore);
 
       printf("Play Again? Press 'y' and hit ENTER");
-      scanf(" %c", & continueGame);
-      if (continueGame == 'y')
+      //scanf(" %c", & continueGame);
+		continueGame = proceedGame();
+      if (continueGame)
         resetMatch( & turn, & gameOver, & continueGame, piecesRemaining, row);
-    } else {
+      } 
+	  else {
       turn = turn ^ 1; //toggle turn        
       printf("Continue? Press 'y' and hit ENTER");
-      scanf(" %c", & continueGame);
+      continueGame = proceedGame();
     }
 
   }
   //game finished or user exited
 
-  if (continueGame != 'y')
+  if (!continueGame)
     printf("User Exited Game");
 
   return 0;
@@ -135,11 +138,56 @@ int receivedInput() {
   }
 }
 
-void resetMatch(bool * turn, bool * gameOver, char * continueGame, int * piecesRemaining, int row[BOARD_SIZE][BOARD_SIZE]) {
+bool proceedGame() {
+  /* Declare volatile pointers to I/O registers (volatile means that IO load
+  and store instructions will be used to access these pointer locations,
+  instead of regular memory loads and stores) */
+  volatile int * PS2_ptr = (int * ) 0xFF200100;
+  int PS2_data, RVALID;
+  char byte1 = 0, byte2 = 0, byte3 = 0;
+  // PS/2 mouse needs to be reset (must be already plugged in)
+  *(PS2_ptr) = 0xFF; // reset
+
+  int letterCode;
+  bool inputFinished = false;
+  while (!inputFinished) {
+    PS2_data = * (PS2_ptr); // read the Data register in the PS/2 port
+    RVALID = PS2_data & 0x8000; // extract the RVALID field
+
+    if (RVALID) {
+      /* shift the next data byte into the display */
+      byte1 = byte2;
+      byte2 = byte3;
+      byte3 = PS2_data & 0xFF;
+      if ((byte2 == (char) 0xAA) && (byte3 == (char) 0x00))
+        // mouse inserted; initialize sending of data
+        *
+        (PS2_ptr) = 0xF4;
+      unsigned int shift_buffer[3];
+      shift_buffer[2] = byte3;
+      //printf("\nvalue is : %d", shift_buffer[2]);
+      shift_buffer[1] = byte2;
+      //printf("\nprev_value is : %d", shift_buffer[1]);
+      shift_buffer[0] = byte1;
+      //printf("\nprev2_value is : %d", shift_buffer[0]);
+
+      if (byte1 == 90 && byte2 == 240 && byte3 == 90)
+        inputFinished = true;
+      else if (byte3 != 240 && byte3 != 90)
+        letterCode = byte3;
+    }
+  }
+
+  if (letterCode == 170) //if q is pressed
+	  return false;
+  return true;
+}
+
+void resetMatch(bool * turn, bool * gameOver, bool * continueGame, int * piecesRemaining, int row[BOARD_SIZE][BOARD_SIZE]) {
 
   * turn = true;
   * gameOver = false;
-  * continueGame = 'y';
+  * continueGame = true;
 
   //Randomize the distibution of the 21 pieces across the piles
   int totalCount = 21;
